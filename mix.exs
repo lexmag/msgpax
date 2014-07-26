2ba1d28d6e091aaec3e0fcc65e074fb1ab9ebc70
Aleksei Magusev <lexmag@me.com>
2014-07-27 00:59:59 +0400

    Start brand new version

--- mix.exs
+++ mix.exs
@@ -2,13 +2,10 @@ defmodule MessagePack.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :message_pack,
-      version: "0.1.0",
-      elixir: "~> 0.11",
-      deps: deps ]
+    [app: :message_pack,
+     version: "0.2.0",
+     elixir: "~> 0.11"]
   end
 
   def application, do: []
-
-  defp deps, do: []
 end
