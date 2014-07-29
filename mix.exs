e7300f93dbdbda419ffd3285caf3ae3721c761f3
Aleksei Magusev <lexmag@me.com>
2014-07-29 22:13:17 +0400

    Publish to Hex

--- mix.exs
+++ mix.exs
@@ -4,8 +4,22 @@ defmodule Msgpax.Mixfile do
   def project do
     [app: :msgpax,
      version: "0.2.0",
-     elixir: "~> 0.14.3"]
+     elixir: "~> 0.14.3",
+     description: description,
+     package: package]
   end
 
   def application, do: []
+
+  defp description do
+    "This library provides an API for serializing" <>
+    "and de-serializing Elixir terms using the MessagePack format."
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
+     contributors: ["Aleksei Magusev"],
+     licenses: ["ISC"],
+     links: %{"GitHub" => "https://github.com/lexmag/msgpax"}]
+  end
 end
