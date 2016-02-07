b01b2ed9a87a241ed47809348f51d6c108bd0cfc
Andrea Leopardi <an.leopardi@gmail.com>
2016-02-07 16:22:52 +0100

    Add docs to the Msgpax.Ext module
    I added both a moduledoc for the module and docs for the `new/2`
    function.

--- lib/msgpax/ext.ex
+++ lib/msgpax/ext.ex
@@ -1,6 +1,57 @@
 defmodule Msgpax.Ext do
+  @moduledoc """
+  A struct used to represent a [MessagePack
+  extension](https://github.com/msgpack/msgpack/blob/master/spec.md#formats-ext).
+
+  ## Examples
+
+  Let's say we want to be able to serialize a custom type that consists of a
+  byte `b` repeated `reps` times. We could represent this as a `RepByte` struct
+  in Elixir:
+
+      defmodule RepByte do
+        defstruct [:b, :reps]
+      end
+
+  A simple (albeit not space efficient) approach to encoding such data is simply
+  a binary containing `b` for `reps` times: `%RepByte{str: ?a, reps: 2}` would be
+  encoded as `"aa"`.
+
+  We can now define the `Msgpax.Packer` protocol for the `RepByte` struct to
+  tell `Msgpax` how to encode this struct (we'll choose `10` as an arbitrary
+  integer to identify the type of this extension).
+
+      defimpl Msgpax.Packer, for: RepByte do
+        def transform(%RepByte{b: b, reps: reps}) do
+          Msgpax.Ext.new(10, String.duplicate(<<b>>, reps))
+          |> Msgpax.Packer.transform()
+        end
+      end
+
+  Now, we can pack `RepByte`s:
+
+      iex> packed = Msgpax.pack!(%RepByte{b: ?a, reps: 3})
+      iex> Msgpax.unpack!(packed)
+      #Msgpax.Ext<10, "aaa">
+
+  """
+
   defstruct [:type, :data]
 
+  @doc """
+  Creates a new `Msgpax.Ext` struct.
+
+  `type` must be an integer in `0..127` and it will be used as the type of the
+  extension (whose meaning depends on your application). `data` must be a binary
+  containing the serialized extension (whose serialization depends on your
+  application).
+
+  ## Examples
+
+      iex> Msgpax.Ext.new(24, "foo")
+      #Msgpax.Ext<24, "foo">
+
+  """
   def new(type, data)
       when type in 0..127 and is_binary(data) do
     %__MODULE__{type: type, data: data}
