5d6aa3f4681b2e1a41ddcad884f7aa5f285cc0f4
Aleksei Magusev <lexmag@me.com>
2016-02-14 17:29:34 +0100

    Drop support for pre-1.0 Elixir versions

--- mix.exs
+++ mix.exs
@@ -4,28 +4,28 @@ defmodule Msgpax.Mixfile do
   def project do
     [app: :msgpax,
      version: "0.8.1",
-     elixir: ">= 0.15.0",
+     elixir: "~> 1.0",
      consolidate_protocols: Mix.env != :test,
-     description: description,
-     deps: deps,
-     package: package]
+     description: description(),
+     deps: deps(),
+     package: package()]
   end
 
-  def application, do: []
+  def application(), do: []
 
-  defp description do
+  defp description() do
     "This library provides an API for serializing" <>
-    " and de-serializing Elixir terms using the MessagePack format."
+    " and de-serializing Elixir terms using the MessagePack format"
   end
 
-  defp package do
+  defp package() do
     [files: ["lib", "mix.exs", "README.md", "LICENSE"],
      contributors: ["Aleksei Magusev"],
      licenses: ["ISC"],
      links: %{"GitHub" => "https://github.com/lexmag/msgpax"}]
   end
 
-  defp deps do
+  defp deps() do
     [{:earmark, ">= 0.0.0", only: :docs},
      {:ex_doc, ">= 0.0.0", only: :docs}]
   end
