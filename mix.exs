38304c277d923e2abfce33ef402fc69228583414
Aleksei Magusev <lexmag@me.com>
2018-02-02 12:58:07 +0100

    Update ExDoc dependency

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule Msgpax.Mixfile do
   end
 
   defp deps() do
-    [{:ex_doc, "~> 0.14", only: :dev, runtime: false},
+    [{:ex_doc, "~> 0.18", only: :dev, runtime: false},
      {:plug, "~> 1.0", optional: true}]
   end
 end
