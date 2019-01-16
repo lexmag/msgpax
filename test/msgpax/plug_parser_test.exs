da4f1c7e5cf2d8174dc985a7c23b31f65607ead1
Aleksei Magusev <lexmag@me.com>
2019-01-16 13:54:51 +0100

    Use :iodata option in PlugParser tests

--- test/msgpax/plug_parser_test.exs
+++ test/msgpax/plug_parser_test.exs
@@ -3,34 +3,34 @@ defmodule Msgpax.PlugParserTest do
   use Plug.Test
 
   test "body with a MessagePack-encoded map" do
-    conn = conn(:post, "/", Msgpax.pack!(%{hello: "world"}) |> IO.iodata_to_binary())
+    conn = conn(:post, "/", Msgpax.pack!(%{hello: "world"}, iodata: false))
 
     assert {:ok, %{"hello" => "world"}, _conn} = parse(conn, [])
   end
 
   test "body with a MessagePack-encoded non-map term" do
-    conn = conn(:post, "/", Msgpax.pack!(100) |> IO.iodata_to_binary())
+    conn = conn(:post, "/", Msgpax.pack!(100, iodata: false))
 
     assert {:ok, %{"_msgpack" => 100}, _conn} = parse(conn, [])
   end
 
   test "accepts an MFA for options" do
     binary = Msgpax.Bin.new("hello world")
-    conn = conn(:post, "/", Msgpax.pack!(binary) |> IO.iodata_to_binary())
+    conn = conn(:post, "/", Msgpax.pack!(binary, iodata: false))
 
     assert {:ok, unpacked, _conn} = parse(conn, unpacker: {Msgpax, :unpack!, [[binary: true]]})
     assert unpacked == binary
   end
 
   test "accepts a module for options" do
-    conn = conn(:post, "/", Msgpax.pack!(100) |> IO.iodata_to_binary())
+    conn = conn(:post, "/", Msgpax.pack!(100, iodata: false))
 
     assert {:ok, unpacked, _conn} = parse(conn, unpacker: Msgpax)
-    assert {:ok, %{"_msgpack" => 100}, _conn} = parse(conn, [])
+    assert {:ok, ^unpacked, _conn} = parse(conn, [])
   end
 
   test "request with a content-type other than application/msgpack" do
-    conn = conn(:post, "/", Msgpax.pack!(100) |> IO.iodata_to_binary())
+    conn = conn(:post, "/", Msgpax.pack!(100, iodata: false))
     options = Msgpax.PlugParser.init([])
 
     assert {:next, ^conn} = Msgpax.PlugParser.parse(conn, "application", "json", %{}, options)
