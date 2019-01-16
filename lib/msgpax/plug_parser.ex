d142018ba7657e4993e1266a8da177d3fc2c89ba
Cẩm Huỳnh <huynhquancam@gmail.com>
2019-01-16 13:52:18 +0100

    Accept :unpacker option in PlugParser (#44)

--- lib/msgpax/plug_parser.ex
+++ lib/msgpax/plug_parser.ex
@@ -7,13 +7,27 @@ if Code.ensure_compiled?(Plug) do
     `Plug.Parsers`](http://hexdocs.pm/plug/Plug.Parsers.html) for more
     information on how to use `Plug.Parsers`.
 
+    This parser accepts the `:unpacker` option to configure how unpacking should be done.
+    Its value can either be a module that implements the `unpack!/1` function
+    or a module, function, and arguments tuple. Note, the response
+    body will be prepended to the given list of arguments before applying.
+
     ## Examples
 
         defmodule MyPlugPipeline do
           use Plug.Builder
 
-          plug Plug.Parsers, parsers: [Msgpax.PlugParser], pass: ["application/msgpack"]
-          # rest of the pipeline
+          plug Plug.Parsers,
+               parsers: [Msgpax.PlugParser],
+               pass: ["application/msgpack"]
+
+          # Or use the :unpacker option:
+          plug Plug.Parsers,
+               parsers: [Msgpax.PlugParser],
+               pass: ["application/msgpack"],
+               unpacker: {Msgpax, :unpack!, [binary: true]}
+
+          # ... rest of the pipeline
         end
 
     """
@@ -22,30 +36,75 @@ if Code.ensure_compiled?(Plug) do
 
     import Plug.Conn
 
-    def parse(%Plug.Conn{} = conn, "application", "msgpack", _headers, opts) do
-      case read_body(conn, opts) do
+    def parse(%Plug.Conn{} = conn, "application", "msgpack", _params, {unpacker, options}) do
+      case read_body(conn, options) do
         {:ok, body, conn} ->
-          {:ok, unpack_body(body), conn}
+          {:ok, unpack_body(body, unpacker), conn}
 
         {:more, _partial_body, conn} ->
           {:error, :too_large, conn}
       end
     end
 
-    def parse(%Plug.Conn{} = conn, _type, _subtype, _headers, _opts) do
+    def parse(%Plug.Conn{} = conn, _type, _subtype, _params, _opts) do
       {:next, conn}
     end
 
-    def init(opts), do: opts
+    def init(options) do
+      {unpacker, options} = Keyword.pop(options, :unpacker, Msgpax)
 
-    defp unpack_body(body) do
-      case Msgpax.unpack!(body) do
+      validate_unpacker!(unpacker)
+
+      {unpacker, options}
+    end
+
+    defp unpack_body(body, unpacker) do
+      case apply_mfa_or_module(body, unpacker) do
         data when is_map(data) -> data
         data -> %{"_msgpack" => data}
       end
     rescue
-      exception in [Msgpax.UnpackError] ->
+      exception ->
         raise Plug.Parsers.ParseError, exception: exception
+    end
+
+    defp apply_mfa_or_module(body, {module, function, extra_args}) do
+      apply(module, function, [body | extra_args])
+    end
+
+    defp apply_mfa_or_module(body, unpacker) do
+      unpacker.unpack!(body)
+    end
+
+    defp validate_unpacker!({module, function, extra_args})
+         when is_atom(module) and is_atom(function) and is_list(extra_args) do
+      arity = length(extra_args) + 1
+
+      unless Code.ensure_compiled?(module) and function_exported?(module, function, arity) do
+        raise ArgumentError,
+              "invalid :unpacker option. Undefined function " <>
+                Exception.format_mfa(module, function, arity)
+      end
+    end
+
+    defp validate_unpacker!(unpacker) when is_atom(unpacker) do
+      unless Code.ensure_compiled?(unpacker) do
+        raise ArgumentError,
+              "invalid :unpacker option. The module #{inspect(unpacker)} is not " <>
+                "loaded and could not be found"
+      end
+
+      unless function_exported?(unpacker, :unpack!, 1) do
+        raise ArgumentError,
+              "invalid :unpacker option. The module #{inspect(unpacker)} must " <>
+                "implement unpack!/1"
+      end
+    end
+
+    defp validate_unpacker!(unpacker) do
+      raise ArgumentError,
+            "the :unpacker option expects a module, or a three-element " <>
+              "tuple in the form of {module, function, extra_args}, got: #{inspect(unpacker)}"
     end
   end
 end
--- test/msgpax/plug_parser_test.exs
+++ test/msgpax/plug_parser_test.exs
@@ -5,27 +5,42 @@ defmodule Msgpax.PlugParserTest do
   test "body with a MessagePack-encoded map" do
     conn = conn(:post, "/", Msgpax.pack!(%{hello: "world"}) |> IO.iodata_to_binary())
 
-    assert {:ok, %{"hello" => "world"}, _conn} =
-             Msgpax.PlugParser.parse(conn, "application", "msgpack", [], [])
+    assert {:ok, %{"hello" => "world"}, _conn} = parse(conn, [])
   end
 
   test "body with a MessagePack-encoded non-map term" do
     conn = conn(:post, "/", Msgpax.pack!(100) |> IO.iodata_to_binary())
 
-    assert {:ok, %{"_msgpack" => 100}, _conn} =
-             Msgpax.PlugParser.parse(conn, "application", "msgpack", [], [])
+    assert {:ok, %{"_msgpack" => 100}, _conn} = parse(conn, [])
+  end
+
+  test "accepts an MFA for options" do
+    binary = Msgpax.Bin.new("hello world")
+    conn = conn(:post, "/", Msgpax.pack!(binary) |> IO.iodata_to_binary())
+
+    assert {:ok, unpacked, _conn} = parse(conn, unpacker: {Msgpax, :unpack!, [[binary: true]]})
+    assert unpacked == binary
+  end
+
+  test "accepts a module for options" do
+    conn = conn(:post, "/", Msgpax.pack!(100) |> IO.iodata_to_binary())
+
+    assert {:ok, unpacked, _conn} = parse(conn, unpacker: Msgpax)
+    assert {:ok, %{"_msgpack" => 100}, _conn} = parse(conn, [])
   end
 
   test "request with a content-type other than application/msgpack" do
     conn = conn(:post, "/", Msgpax.pack!(100) |> IO.iodata_to_binary())
-    assert {:next, ^conn} = Msgpax.PlugParser.parse(conn, "application", "json", [], [])
+    options = Msgpax.PlugParser.init([])
+
+    assert {:next, ^conn} = Msgpax.PlugParser.parse(conn, "application", "json", %{}, options)
   end
 
   test "bad MessagePack-encoded body" do
     conn = conn(:post, "/", "bad body")
 
     assert_raise Plug.Parsers.ParseError, ~r/found excess bytes/, fn ->
-      Msgpax.PlugParser.parse(conn, "application", "msgpack", [], [])
+      parse(conn, [])
     end
   end
 
@@ -33,7 +48,12 @@ defmodule Msgpax.PlugParserTest do
     conn = conn(:post, "/", "")
 
     assert_raise Plug.Parsers.ParseError, ~r/given binary is incomplete/, fn ->
-      Msgpax.PlugParser.parse(conn, "application", "msgpack", [], [])
+      parse(conn, [])
     end
+  end
+
+  defp parse(conn, options) do
+    options = Msgpax.PlugParser.init(options)
+    Msgpax.PlugParser.parse(conn, "application", "msgpack", %{}, options)
   end
 end
